f710ec0824b4dcedafc942b2cffda44e64ae9b42
ZachPinet <zachpinet@zachpi.net>
2026-03-06 16:06:32 -0500

    feat: Add LRU

--- src/main.py
+++ src/main.py
@@ -2,6 +2,20 @@ from pathlib import Path
 from collections import deque
 
 
+# This reads an input file to extract its values.
+def read_file(filepath):
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+        
+    # The first line gets k and m.
+    k, m = map(int, lines[0].strip().split())
+    
+    # The second line gets all the requests.
+    reqs = list(map(int, lines[1].strip().split()))
+    
+    return k, m, reqs
+
+
 # This is the First-In, First-Out policy.
 def fifo(k, reqs):
     cache = deque()
@@ -27,18 +41,31 @@ def fifo(k, reqs):
     return misses
 
 
-# This reads an input file to extract its values.
-def read_file(filepath):
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
-        
-    # The first line gets k and m.
-    k, m = map(int, lines[0].strip().split())
+# This is the Least Recently Used policy.
+def lru(k, reqs):
+    lru_dict = {}
+    misses = 0
+
+    for req in reqs:
+        # Increment each value by 1.
+        for r in lru_dict:
+            lru_dict[r] += 1
+
+        # Set existing pair's value back to 0.
+        if req in lru_dict:
+            lru_dict[req] = 0
+            continue
+        else:
+            misses += 1
+            if len(lru_dict) < k:
+                lru_dict[req] = 0
+            # Remove the pair with the highest value.
+            else:
+                lru_key = max(lru_dict, key=lambda k: lru_dict[k])
+                lru_dict.pop(lru_key)
+                lru_dict[req] = 0
     
-    # The second line gets all the requests.
-    reqs = list(map(int, lines[1].strip().split()))
-    
-    return k, m, reqs
+    return misses
 
 
 # This is the main function.
@@ -52,11 +79,13 @@ def main():
         
         k, m, reqs = read_file(input_file)
         fifo_misses = fifo(k, reqs)
+        lru_misses = lru(k, reqs)
 
         # Create a new output file for each input file.
         output_file = outputs_dir / f'{input_file.name[:-4]}_output.txt'
         with open(output_file, 'w') as f:
             f.write(f"FIFO  : {fifo_misses}\n")
+            f.write(f"LRU   : {lru_misses}\n")
 
 
 if __name__ == "__main__":
