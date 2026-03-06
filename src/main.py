bf7edd034ae8c007b2b0f460edadb61b523fecca
ZachPinet <zachpinet@zachpi.net>
2026-03-06 17:36:16 -0500

    feat: Add OPTFF

--- src/main.py
+++ src/main.py
@@ -43,28 +43,80 @@ def fifo(k, reqs):
 
 # This is the Least Recently Used policy.
 def lru(k, reqs):
-    lru_dict = {}
+    cache = {}
     misses = 0
 
     for req in reqs:
         # Increment each value by 1.
-        for r in lru_dict:
-            lru_dict[r] += 1
+        for r in cache:
+            cache[r] += 1
 
         # Set existing pair's value back to 0.
-        if req in lru_dict:
-            lru_dict[req] = 0
+        if req in cache:
+            cache[req] = 0
             continue
         else:
             misses += 1
-            if len(lru_dict) < k:
-                lru_dict[req] = 0
+            if len(cache) < k:
+                cache[req] = 0
             # Remove the pair with the highest value.
             else:
-                lru_key = max(lru_dict, key=lambda k: lru_dict[k])
-                lru_dict.pop(lru_key)
-                lru_dict[req] = 0
+                lru_key = max(cache, key=lambda k: cache[k])
+                cache.pop(lru_key)
+                cache[req] = 0
     
+    return misses
+
+
+# This is Belady's Farthest-in-Future policy.
+def optff(k, reqs):
+    # Store the positions of all requests.
+    all_pos_dict = {}
+    i = 0
+    for req in reqs:
+        if req in all_pos_dict:
+            all_pos_dict[req].append(i)
+        else:
+            all_pos_dict[req] = [i]
+        i += 1
+
+    cache = set()
+    misses = 0
+    i = 0
+    for req in reqs:
+        if req in cache:
+            i += 1
+            continue
+        else:
+            misses += 1
+            if len(cache) < k:
+                cache.add(req)
+            # Find the req with the highest next pos after i.
+            else:
+                curr_evicted = None
+                curr_max = i
+                for r in cache:
+                    has_future_pos = False
+                    for j in all_pos_dict[r]:
+                        # Find the first position after i.
+                        if j > i:
+                            if j > curr_max:
+                                curr_evicted = r
+                                curr_max = j
+                            has_future_pos = True
+                            break
+                    
+                    # If no position after i, select r for removal.
+                    if has_future_pos == False:
+                        curr_evicted = r
+                        break
+
+                # Remove the request from the cache.
+                cache.remove(curr_evicted)
+                cache.add(req)
+
+            i += 1
+
     return misses
 
 
@@ -80,12 +132,14 @@ def main():
         k, m, reqs = read_file(input_file)
         fifo_misses = fifo(k, reqs)
         lru_misses = lru(k, reqs)
+        optff_misses = optff(k, reqs)
 
         # Create a new output file for each input file.
         output_file = outputs_dir / f'{input_file.name[:-4]}_output.txt'
         with open(output_file, 'w') as f:
             f.write(f"FIFO  : {fifo_misses}\n")
             f.write(f"LRU   : {lru_misses}\n")
+            f.write(f"OPTFF : {optff_misses}")
 
 
 if __name__ == "__main__":
